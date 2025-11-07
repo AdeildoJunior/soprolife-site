1d2a4c5fe02a6045d9be036186b43642e7558c11
Adeildo de Lima <adeildolimajr@gmail.com>
2025-11-07 16:22:38 -0300

    seo(og+twitter+jsonld) + tel clicável; SW bump (sl-20251107162238)

--- sw.js
+++ sw.js
@@ -1,4 +1,4 @@
-const CACHE_NAME = 'sl-20251107161624';
+const CACHE_NAME = 'sl-20251107162238';
 const ASSETS = [ '/', '/index.html', '/manifest.webmanifest', '/icon-192.png', '/icon-512.png', '/maskable-512.png', '/img/whatsapp.svg', '/img/sopro-logo.svg', '/favicon.svg?v=1', '/favicon-32.png?v=2', '/favicon-16.png?v=2', '/apple-touch-icon.png?v=2', '/offline.html' ];
 
 self.addEventListener('install', e => {
